5651d958574e4d865d2466a36ac55213b6adec6c
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-04-24 23:49:49 +0200

    feat: adding security updates

--- packages/frontend/src/setupTests.ts
+++ packages/frontend/src/setupTests.ts
@@ -2,33 +2,4 @@ import "@testing-library/jest-dom";
 
 global.ResizeObserver = require("resize-observer-polyfill");
 
-jest.mock("@cardano-foundation/cardano-connect-with-wallet", () => ({
-  useCardano: () => ({
-    isEnabled: false,
-    isConnected: false,
-    isConnecting: false,
-    enabledWallet: "exampleWallet",
-    stakeAddress: "exampleAddress",
-    usedAddresses: [],
-    unusedAddresses: [],
-    signMessage: jest.fn(),
-    connect: jest.fn(),
-    disconnect: jest.fn(),
-    installedExtensions: [],
-    accountBalance: 100
-  }),
-  NetworkType: {
-    MAINNET: "mainnet",
-    TESTNET: "testnet"
-  }
-}));
-
 jest.mock("*.svg?react", () => "SvgMock");
-
-jest.mock("@cardano-foundation/cardano-connect-with-wallet-core", () => ({
-  NetworkType: {
-    MAINNET: "mainnet",
-    TESTNET: "testnet"
-  },
-  isWalletInstalled: jest.fn()
-}));
